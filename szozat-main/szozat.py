c7212c045a8c060ff27472d94203561e148cfdae
PTR <majercsikpeter@gmail.com>
2022-02-24 22:14:26 +0100

    debug, you can only write in valid letters and colors now
    If you wrote in "3" as a number, it accepted it and made a new wordlist with every word containing "3", so zero. Now you can only write in valid letters. Same for color codes.

--- szozat-main/szozat.py
+++ szozat-main/szozat.py
@@ -1,34 +1,43 @@
-words = []
-letter="a"
-position=1
-wordlist=[]
-with open('szavak.txt', encoding="utf=8") as w:
-  words = w.readlines()
-while letter!='':
-  del wordlist[:]
-  letter=input("Milyen betűt ismersz? ")
-  color=input("Milyen színű? (S,Z,F): ")
-  if color == 'z' or color == 'Z' or color == 's' or color == 'S':
-    position=int(input("Hányadik helyen áll? (1-5)"))-1
-  for word in words:
-    if color == 'z' or color == 'Z':
-      if word[position]==letter:
-        wordlist.append(word)
-    elif color == 'f' or color == 'F':
-      if letter not in word:
-        wordlist.append(word)
-    elif color == 's' or color == 'S':
-      if letter in word:
-        if word[position]!=letter:
+def szozat():
+  words = []
+  letter="a"
+  position=1
+  wordlist=[]
+  with open('szavak.txt', encoding="utf=8") as w:
+    words = w.readlines()
+  while letter!='':
+    del wordlist[:]
+    letter=input("Milyen betűt ismersz? ")
+    while letter not in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "í", "é", "á", "ű", "ő", "ú", "ö", "ü", "ó"]:
+      print(f"A(z) {letter} nem egy betű, próbáld újra!")
+      letter = input("Milyen betűt ismersz? ")
+    color=input("Milyen színű? (S,Z,F): ")
+    while color not in ["F", "f", "Z", "z", "S", "s"]:
+      print(f"A(z) {color} nem a szín kezdőbetűje. Kérlek írd be a helyes szín kezdőbetűjét! (s, f, z)")
+      color = input("Milyen színű? (S, Z, F vagy s, z, f) ")
+    if color == 'z' or color == 'Z' or color == 's' or color == 'S':
+      position=int(input("Hányadik helyen áll? (1-5)"))-1
+    for word in words:
+      if color == 'z' or color == 'Z':
+        if word[position]==letter:
           wordlist.append(word)
-  words=wordlist.copy()
-  print(*wordlist)
-  print(f"Lehetséges helyes szavak: {len(words)} db")
+      elif color == 'f' or color == 'F':
+        if letter not in word:
+          wordlist.append(word)
+      elif color == 's' or color == 'S':
+        if letter in word:
+          if word[position]!=letter:
+            wordlist.append(word)
+    words=wordlist.copy()
+    print(*wordlist)
+    print(f"Lehetséges helyes szavak: {len(words)} db")
 
-  if len(words) < 1:
-    print("Sajnos a szó valószínűleg kettős betűt (sz, cs, gy...) tartalmaz, amit egyelőre nem támogat a program. \nVagy nincs benne a program szótárában. ¯\_(ツ)_/¯")
-    #uj_szotar = print(input("Úgy néz ki nincs a szó a szótárban, vagy dupla betű (sz, gy, stb...) van benne. Szeretnéd megpróbálni a dupla betűs szótárral? I/N "))
-    #if uj_szotar == "I":
-      #print("oké próbáljuk meg!")
-    #else:
-      #print("rip. legalább a funkció működik. (:")
+    if len(words) < 1:
+      print("Sajnos a szó valószínűleg kettős betűt (sz, cs, gy...) tartalmaz, amit egyelőre nem támogat a program. \nVagy nincs benne a program szótárában. ¯\_(ツ)_/¯")
+      #uj_szotar = print(input("Úgy néz ki nincs a szó a szótárban, vagy dupla betű (sz, gy, stb...) van benne. Szeretnéd megpróbálni a dupla betűs szótárral? I/N "))
+      #if uj_szotar == "I":
+        #print("oké próbáljuk meg!")
+      #else:
+        #print("rip. legalább a funkció működik. (:")
+
+szozat()
