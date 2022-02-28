8ffec4c7394905f9dd337d7cc3f9d89c3499e033
PTR <majercsikpeter@gmail.com>
2022-02-28 17:58:00 +0100

    deleted unnessesarry files

--- 6betus/szo_valogato.py
+++ 6betus/szo_valogato.py
@@ -1,10 +1,9 @@
 words = []
 dupla = []
-with open("6_betus_szavak.txt", encoding="utf=8") as w:
+with open("magyar-szavak.txt", encoding="utf=8") as w:
     words = w.readlines()
 
-for word in words:
-    if "sz" in word or "cs" in word or "dz" in word or "gy" in word or "ly" in word or "ny" in word or "ty" in word or "zs" in word:
-        dupla.append(word)
+szett = set(words)
+abc_szett = sorted(szett)
 
-print(" ".join(dupla).lower())
+print(*abc_szett)
