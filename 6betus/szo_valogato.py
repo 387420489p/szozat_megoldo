7cfe9c965fc82c32c519af802c93f15f736181ef
PTR <majercsikpeter@gmail.com>
2022-03-13 00:36:01 +0100

    bugfix, not allowing symbols in words

--- 6betus/szo_valogato.py
+++ 6betus/szo_valogato.py
@@ -1,15 +1,14 @@
 words = []
-letter = "a"
-position = 1
 wordlist = []
 with open("magyar-szavak.txt", encoding="utf=8") as r:
     words = r.readlines()
 
 for word in words:
-    if len(word) == 6:
+    word = word.strip()
+    if len(word) == 5 and word.isalpha():
         if "sz" not in word and "cs" not in word and "dz" not in word and "gy" not in word and "ly" not in word and "ny" not in word and "ty" not in word and "zs" not in word:
             wordlist.append(word)
-    elif len(word) == 7:
+    elif len(word) == 6 and word.isalpha():
         if "sz" in word or "cs" in word or "dz" in word or "gy" in word or "ly" in word or "ny" in word or "ty" in word or "zs" in word:
             wordlist.append(word)
     else:
@@ -18,6 +17,6 @@ for word in words:
 wordlist.sort()
 
 with open("osszes_szo.txt", "w", encoding="utf-8") as w:
-    w.write("".join(wordlist).lower())
+    w.write("\n".join(wordlist).lower())
 
-print("".join(wordlist).lower())
+print("\n".join(wordlist).lower())
