77490e8d326238d67f6e92a0372969b4bb9ee1a7
PTR <majercsikpeter@gmail.com>
2022-04-05 19:41:04 +0200

    deleted unnessesary for loop

--- 6betus/szo_valogato.py
+++ 6betus/szo_valogato.py
@@ -13,32 +13,31 @@ for word in words:
         if "sz" in word or "cs" in word or "dz" in word or "gy" in word or "ly" in word or "ny" in word or "ty" in word or "zs" in word:
             wordlist.append(word)
     elif len(word) == 7 and word.isalpha():
-        for dupla in word:
-            if "sz" in word:
-                i += 1
-            if "dz" in word:
-                i += 1
-            if "gy" in word:
-                i += 1
-            if "ly" in word:
-                i += 1
-            if "ny" in word:
-                i += 1
-            if "ty" in word:
-                i += 1
-            if "zs" in word:
-                i += 1
-            if "cs" in word:
-                i += 1
-            if i == 7*2:
-                wordlist.append(word)
+        if "sz" in word:
+            i += 1
+        if "dz" in word:
+            i += 1
+        if "gy" in word:
+            i += 1
+        if "ly" in word:
+            i += 1
+        if "ny" in word:
+            i += 1
+        if "ty" in word:
+            i += 1
+        if "zs" in word:
+            i += 1
+        if "cs" in word:
+            i += 1
+        if i == 2:
+            wordlist.append(word)
         i = 0
     else:
         pass
 
 wordlist.sort()
 
-with open("osszes_szo.txt", "w", encoding="utf-8") as w:
-    w.write("\n".join(wordlist).lower())
+# with open("osszes_szo.txt", "w", encoding="utf-8") as w:
+#     w.write("\n".join(wordlist).lower())
 
 print("\n".join(wordlist).lower())
